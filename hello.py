ee5db94e1dee94e63a3eafed292bb4939aadad40
danielroblem <daniel12dar@gmail.com>
2013-10-21 10:45:18 -0300

    Agregado Rutas

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 
 
 import os
-from flask import Flask
+from flask import Flask ,jsonify
 
 app = Flask(__name__)
 
@@ -9,3 +9,20 @@ app = Flask(__name__)
 def hello():
     return 'Hello World! 2'
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+@app.route('API/V0.1/', methods = ['GET'])
+def get_tasks():
+    return jsonify( { 'tasks': tasks } )
