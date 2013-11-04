4a6c17326a0c8cafdcd874eb87c9b075b7219ddf
danielroblem <daniel12dar@gmail.com>
2013-11-04 14:56:55 -0300

    test6

--- hello.py
+++ hello.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello World! 2'
+    return 'Hello World! 3'
 tweet=TwitterCore.getTweet()
 # print tweet
 #create dict
@@ -29,8 +29,28 @@ print x
 # A=json_myobj.iOStweet('ame','text','profile_image_url','retweet_count')
 # A=json.loads(tweet)
 # print A
-@app.before_request
+
 @app.route('/api/v0.1/tasks', methods = ['GET'])
+@app.before_request
 def get_tasks(): 
-    return jsonify(x)
+	tweet=TwitterCore.getTweet()
+ # print tweet
+ #create dict
+ 	x={}
+ 	for i in range (len(tweet)):
+   	  Dict={
+        'tweet_number  %s'%i:{'text':tweet[i]._tweetText,
+       	'Author':tweet[i]._Author}
+      	}
+     # print Dict 
+     	x.update(Dict) 
+     
+ 	# print jsonify(x)
+ 	print x
+ 	# print tweet[0]._tweetText
+	# print tweet[1]._tweetText
+	# A=json_myobj.iOStweet('ame','text','profile_image_url','retweet_count')
+	# A=json.loads(tweet)
+	# print A
+	return jsonify(x)
 
